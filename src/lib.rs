d1297810c5d91499e40b9a3a70a3c0c0b0a3159b
hanxx <xsytby1112@gmail.com>
2022-08-08 22:48:40 +0800

    style: fix rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub fn now() -> i64 {
   match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
     Ok(n) => match i64::try_from(n.as_micros()) {
       Ok(n) => n,
-      Err(_) => panic!("[microtime.now] u128 microsecond cannot convert to i64")
+      Err(_) => panic!("[microtime.now] u128 microsecond cannot convert to i64"),
     },
     Err(_) => panic!("[microtime.now] SystemTime before UNIX EPOCH!"),
   }
